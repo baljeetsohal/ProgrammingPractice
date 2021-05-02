f036a76916f650059062acdac95c7a282de36d67
baljeetsohal <baljeet.sohal106@gmail.com>
2021-05-03 00:29:20 +0530

    Create Primary key(Address_id)in foreign table

--- HibernatePrac/src/main/java/com/baljeet/coding/HibernatePrac/model/User.java
+++ HibernatePrac/src/main/java/com/baljeet/coding/HibernatePrac/model/User.java
@@ -1,5 +1,7 @@
 package com.baljeet.coding.HibernatePrac.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -10,10 +12,17 @@ import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
+
+import org.hibernate.annotations.CollectionId;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.hibernate.mapping.Array;
 
 @Entity(name="USER_DETAILS")
 @Table(name="USERDETAILS")
@@ -24,7 +33,10 @@ public class User {
 	@Column(name="username")
 	private String name;
 	@ElementCollection
-	private Set<Address> listofAddress=new HashSet<Address>();
+	@JoinTable(name="User_Address",joinColumns=@JoinColumn(name="User_id"))
+	@GenericGenerator(name="increment-gen",strategy="increment")
+	@CollectionId(columns = { @Column(name="Address_id") }, generator = "increment-gen", type = @Type( type = "long"))
+	private Collection<Address> listofAddress=new ArrayList<Address>();
 	
 	public String getName() {
 		return name;
@@ -47,15 +59,20 @@ public class User {
 	}
 
 
-
-	public Set<Address> getListofAddress() {
+	public Collection<Address> getListofAddress() {
 		return listofAddress;
 	}
 
 
-	public void setListofAddress(Set<Address> listofAddress) {
+	public void setListofAddress(Collection<Address> listofAddress) {
 		this.listofAddress = listofAddress;
 	}
+
+
+
+
+
+	
 	
 
 
